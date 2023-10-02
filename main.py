7380e9e51ff14ab9bfd982eb0c78af178b9419f7
Nick Neos <nicholas.neos@gmail.com>
2023-10-02 13:22:20 +1100

    fixes

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import os
 import re
 import sys
 from datetime import datetime
-from unicodedata import normalize
+from pathlib import Path
 
 
-PATTERN = r"s?(\d{1,2})\s?[ex]\s?(\d{1,3})(\s?\-?\s?[ex]?(\d{1,3}))?"
+PATTERN = r"s?(\d{1,2})\s?[ex]\s?(\d{1,2})(\s?\-?\s?[ex]?(\d{1,2}))?"
 
 def is_video_file(file):
     extensions = [".avi", ".mp4", ".mkv"]
@@ -26,13 +26,13 @@ def get_tvshow_metadata(file):
     if len(match) <= 0:
         return None, None
     elif len(match) == 1:
-        s = match[0][0]
+        s = int(match[0][0])
         # this is for multi episode files
         # eg. "S01E01-03"
         if match[0][3] == "":
-            e = match[0][1]
+            e = int(match[0][1])
         else:
-            e = [match[0][1], match[0][3]]
+            e = [int(match[0][1]), int(match[0][3])]
     else:
         # also for multi episode files
         # eg. "1x01 & 1x02 & 1x03"
@@ -70,15 +70,18 @@ def build_rename_info(folder):
 
     # generate renamed files
     for dict in tvshow_dicts:
-        # season number as 0 padded string
-        s = f"{dict['season']:02}"
+        try:
+            # season number as 0 padded string
+            s = f"{(dict['season']):02}"
 
-        # episode number as 0 padded string
-        # also handle multi episodes
-        if type(dict["episode"]) is list:
-            e = f"{min(dict['episode']):02}-E{max(dict['episode']):02}"
-        else:
-            e = f"{dict['episode']:02}"
+            # episode number as 0 padded string
+            # also handle multi episodes
+            if type(dict["episode"]) is list:
+                e = f"{min(dict['episode']):02}-E{max(dict['episode']):02}"
+            else:
+                e = f"{dict['episode']:02}"
+        except TypeError:
+            continue
 
         # generate new filename
         new_filename = re.sub(
@@ -100,10 +103,10 @@ def log_to_csv(orig_file, renamed_file, csv_file="history.csv"):
 
     if not os.path.exists(csv_file):
         with open(csv_file, "w") as f:
-            f.write("ts, original_filename, new_filename\n")
+            f.write("ts, folder, original_filename, new_filename\n")
 
     with open(csv_file, "a") as f:
-        f.write(f"{ts}, {orig_file}, {renamed_file}\n")
+        f.write(f"{ts}, {Path(orig_file).parent}, {Path(orig_file).name}, {Path(renamed_file).name}\n")
 
 
 def rename_tvshows(folder, test_run=False):
@@ -112,13 +115,17 @@ def rename_tvshows(folder, test_run=False):
     #     f.write(json.dumps(rename_info, indent=2))
 
     for file in rename_info:
-        file_from = file["file"]
-        file_to = file["rename_to"]
+        try:
+            file_from = file["file"]
+            file_to = file["rename_to"]
+        except KeyError:
+            continue
+
         ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         testing_tag = "[TEST ONLY] " if test_run else ""
 
         # skip if no change in filename
-        if file_from == file_to:
+        if file_from.lower() == file_to.lower():
             continue
 
         # rename tv show file
@@ -126,6 +133,8 @@ def rename_tvshows(folder, test_run=False):
         if not test_run:
             os.rename(file_from, file_to)
             log_to_csv(file_from, file_to)
+        else:
+            log_to_csv(file_from, file_to, "test_runs.csv")
 
 
 if __name__ == "__main__":
